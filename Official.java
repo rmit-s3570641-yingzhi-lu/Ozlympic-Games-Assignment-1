fbfa226a38f31d63c29aa9b3d1a7cd9214fbe6ec
rmit-s3584146-ningqilu <s3584146@student.rmit.edu.au>
2017-03-23 23:38:43 +1100

    Create the abstract class

---
+++ Official.java
@@ -0,0 +1,9 @@
+
+class Official extends Participant {
+
+	public Official(int ID, String name, int age, String state) {
+		super(ID, name, age, state);
+		// TODO Auto-generated constructor stub
+	}
+
+}
