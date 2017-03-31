d704902ed03696732f2ec1e9888ed25feb8c040f
rmit-s3584146-ningqilu <s3584146@student.rmit.edu.au>
2017-03-31 19:33:40 +1100

    Inputting the type to choose the participant list

--- ReadExcel.java
+++ ReadExcel.java
@@ -1,29 +1,58 @@
 package ass1;
-
+/**
+ * @author Ningqi Lu
+ * 27/03/2017
+ */
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
 import jxl.Cell;
 import jxl.Sheet;
 import jxl.Workbook;
 import jxl.read.biff.BiffException;
 
 public class ReadExcel {
+	
+	public static int i;
+	public static int j;
+	public static Workbook book;
+	public static Sheet sheet;
+//	static ArrayList Swimmer = new ArrayList();
 
 	public static void main(String args[]){
-		//The file is allocated at C:\Users\NQ\Desktop.PARTICIPANT.xls
+		//The file is allocated at C:\Users\NQ\Desktop¡£PARTICIPANT.xls
 		File f=new File("C:"+File.separator+"Users"+File.separator+"NQ"+File.separator+"Desktop"+File.separator+"PARTICIPANT.xls");
+	
 		try {
 			//Get the file
 			Workbook book=Workbook.getWorkbook(f);
 			//Get the first sheet
 			Sheet sheet=book.getSheet(0);  
-			for(int i=0;i<sheet.getRows();i++){
-				for(int j=0;j<sheet.getColumns();j++){
+			
+			System.out.println("Input the type(OFFICIAL/SWIMMER/CYCLIST/SPRINTER/SUPERATHLETE):");
+			Scanner sc = new Scanner(System.in);
+			String type = sc.nextLine();
+			
+			for(i = 1; i < sheet.getRows(); i++){
+				for(j = 0; j < sheet.getColumns(); j++){
+					
+				/*	ReadExcel list = new ReadExcel();
+					list.matchSwimmer();*/
+					//Swimmer.add(list);
 					//Get the cells
 					Cell cell=sheet.getCell(j, i);
-					System.out.print(cell.getContents()+" "); 
+					
+					
+					if(cell.getContents().equals(type)){
+							for(int n = 0; n <= j; n++){
+								System.out.print(sheet.getCell(n, i).getContents()+" ");  
+							}
+							System.out.print("\n");
+					}
 				}
-				System.out.print("\n");
+				
 			}
 		} catch (BiffException e) {
 			// TODO Auto-generated catch block
@@ -33,4 +62,16 @@ public class ReadExcel {
 			e.printStackTrace();
 		}
 	}
+	
+/*	public void matchSwimmer(){
+		//Get the cells
+		Cell cell=sheet.getCell(j, i);
+		
+		if(cell.getContents().equals("SWIMMER")){
+				for(int n = 0; n <= j; n++){
+					System.out.print(sheet.getCell(n, i).getContents()+" ");  
+				}
+				System.out.print("\n");
+		}
+	}*/
 	}
