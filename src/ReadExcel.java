52f092157e13b84671ca100c123ca00ba04f5b56
rmit-s3570641-yingzhi-lu <s3570641@student.rmit.edu.au>
2017-03-31 23:23:40 +1100

    Ningqi Lu wrote a class named ReadExcel which could read data with exact type from Excel file Import a jar package

---
+++ src/ReadExcel.java
@@ -0,0 +1,76 @@
+/**
+ * @author Ningqi Lu
+ * 27/03/2017
+ */
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+import jxl.Cell;
+import jxl.Sheet;
+import jxl.Workbook;
+import jxl.read.biff.BiffException;
+
+public class ReadExcel {
+
+    public static int i;
+    public static int j;
+    public static Workbook book;
+    public static Sheet sheet;
+//	static ArrayList Swimmer = new ArrayList();
+
+    public void readExcelData(String type){
+        //The file is allocated at C:\Users\NQ\Desktop¡£PARTICIPANT.xls
+        File f=new File("C:"+File.separator+"Users"+File.separator+"NQ"+File.separator+"Desktop"+File.separator+"PARTICIPANT.xls");
+
+        try {
+            //Get the file
+            Workbook book=Workbook.getWorkbook(f);
+            //Get the first sheet
+            Sheet sheet=book.getSheet(0);
+
+            System.out.println("Input the type(OFFICIAL/SWIMMER/CYCLIST/SPRINTER/SUPERATHLETE):");
+            Scanner sc = new Scanner(System.in);
+            type = sc.nextLine();
+
+            for(i = 1; i < sheet.getRows(); i++){
+                for(j = 0; j < sheet.getColumns(); j++){
+					
+				/*	ReadExcel list = new ReadExcel();
+					list.matchSwimmer();*/
+                    //Swimmer.add(list);
+                    //Get the cells
+                    Cell cell=sheet.getCell(j, i);
+
+
+                    if(cell.getContents().equals(type)){
+                        for(int n = 0; n <= j; n++){
+                            System.out.print(sheet.getCell(n, i).getContents()+" ");
+                        }
+                        System.out.print("\n");
+                    }
+                }
+
+            }
+        } catch (BiffException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
+	
+/*	public void matchSwimmer(){
+		//Get the cells
+		Cell cell=sheet.getCell(j, i);
+		
+		if(cell.getContents().equals("SWIMMER")){
+				for(int n = 0; n <= j; n++){
+					System.out.print(sheet.getCell(n, i).getContents()+" ");  
+				}
+				System.out.print("\n");
+		}
+	}*/
+}
